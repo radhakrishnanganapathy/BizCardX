81c6823217e20220834e563cf174835c737d069c
radhakrishnanganapathy <radhakrishnanganapathyr@gmail.com>
2023-10-25 18:25:48 +0530

    full

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a:5432/guviproject" # manual
 # DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a/guviproject" #internal
-DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a.oregon-postgres.render.com/guviproject" #external
+DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a.oregon-postgres.render.com/guviproject?sslmode=require" #external
 # DATABASE_URI = 'postgresql://postgres:ags009@localhost:5432/curd' #local
 
 
