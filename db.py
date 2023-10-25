120f0661630f05148ad78efd232673d16eecbde6
radhakrishnanganapathy <radhakrishnanganapathyr@gmail.com>
2023-10-25 17:11:27 +0530

    full

--- db.py
+++ db.py
@@ -3,10 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a/guviproject"
-# DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a:5432/guviproject"
-# DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a.oregon-postgres.render.com/guviproject"
-# DATABASE_URI = 'postgresql://postgres:ags009@localhost:5432/curd'
+# DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a:5432/guviproject" # manual
+# DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a/guviproject" #internal
+DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a.oregon-postgres.render.com/guviproject" #external
+# DATABASE_URI = 'postgresql://postgres:ags009@localhost:5432/curd' #local
 
 
 engine = create_engine(DATABASE_URI)
