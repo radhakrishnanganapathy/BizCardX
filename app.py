154a28e9a860aec9189805254e32fe9b977b9a27
radhakrishnanganapathy <radhakrishnanganapathyr@gmail.com>
2023-10-26 17:55:01 +0530

    'teccract'

--- app.py
+++ app.py
@@ -7,13 +7,16 @@ from models import Card
 from db import get_db, CreateTables
 import db
 import pandas as pd
+import cv2
+
 # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Replace with your Tesseract path
 # pytesseract.pytesseract.tessdata_dir_config = '--tessdata-dir "C:\Program Files\Tesseract-OCR\tessdata"'  # Adjust to your setup
 
 image = st.file_uploader("upload your image here", type=["jpg","png","jpeg"])
 CreateTables()
 if image is not None:
-     image = Image.open(image)
+     # image = Image.open(image)
+     image = cv2.imread(image)
      text = pytesseract.image_to_string(image)
      st.write(text)
      name_pattern = r"^[A-Z][a-z]+"
