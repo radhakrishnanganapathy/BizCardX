8bb36c81da430a958583842b8114b1d21d5a8642
radhakrishnanganapathy <radhakrishnanganapathyr@gmail.com>
2023-10-26 17:23:51 +0530

    'teccract'

--- app.py
+++ app.py
@@ -15,37 +15,37 @@ CreateTables()
 if image is not None:
      image = Image.open(image)
      text = pytesseract.image_to_string(image)
-     st.write(text)
-     name_pattern = r"^[A-Z][a-z]+"
-     position_pattern = r"[A-Z][A-Z\s]+"
-     mobile_pattern = r"\+\d{3}-\d{3}-\d{4}"
-     website_pattern = r"www\.[A-Za-z0-9]+\.[a-z]+"
-     email_pattern = r"\S+@\S+"
-     address_pattern = r"\d{1,4}\s[A-Za-z0-9\s,]+\d{6}"
+     # st.write(text)
+     # name_pattern = r"^[A-Z][a-z]+"
+     # position_pattern = r"[A-Z][A-Z\s]+"
+     # mobile_pattern = r"\+\d{3}-\d{3}-\d{4}"
+     # website_pattern = r"www\.[A-Za-z0-9]+\.[a-z]+"
+     # email_pattern = r"\S+@\S+"
+     # address_pattern = r"\d{1,4}\s[A-Za-z0-9\s,]+\d{6}"
 
-     # Extract data using regular expressions
-     name = re.search(name_pattern, text).group()
-     position = re.search(position_pattern, text).group()
-     mobile = re.findall(mobile_pattern, text)
-     website = re.search(website_pattern, text).group() if re.search(website_pattern, text) else None
-     email = re.search(email_pattern, text).group() if re.search(email_pattern, text) else None
-     address = re.search(address_pattern, text).group() if re.search(address_pattern, text) else None
+     # # Extract data using regular expressions
+     # name = re.search(name_pattern, text).group()
+     # position = re.search(position_pattern, text).group()
+     # mobile = re.findall(mobile_pattern, text)
+     # website = re.search(website_pattern, text).group() if re.search(website_pattern, text) else None
+     # email = re.search(email_pattern, text).group() if re.search(email_pattern, text) else None
+     # address = re.search(address_pattern, text).group() if re.search(address_pattern, text) else None
 
-#      lines = text.split('\n')
+     lines = text.split('\n')
 
-# # Extract components based on the structure of the text
-#      name = lines[0]
-#      position = lines[1]
-#      mobile = [line for line in lines if line.startswith('& +')][0].strip()
-#      email = [line for line in lines if line.startswith('~ ')][0][2:].strip()
-#      website = [line for line in lines if line.startswith('Q www.')][0].strip()
+# Extract components based on the structure of the text
+     name = lines[0]
+     position = lines[1]
+     mobile = [line for line in lines if line.startswith('& +')][0].strip()
+     email = [line for line in lines if line.startswith('~ ')][0][2:].strip()
+     website = [line for line in lines if line.startswith('Q www.')][0].strip()
 
-# # Address is a bit more complex, but you can join lines until you reach the postal code
-#      address = ""
-#      for line in lines[lines.index(website) + 1:]:
-#           if any(char.isdigit() for char in line):
-#                break
-#           address += line + ' '
+# Address is a bit more complex, but you can join lines until you reach the postal code
+     address = ""
+     for line in lines[lines.index(website) + 1:]:
+          if any(char.isdigit() for char in line):
+               break
+          address += line + ' '
           # Print the extracted information
      st.write("Name:", name)
      st.write("Position:", position)
