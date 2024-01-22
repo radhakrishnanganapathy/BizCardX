f058d38a6a80b66a4f0caebefebf424804a7f80b
Radhakrishhnan Ganapathy <85297693+radhakrishnanganapathy@users.noreply.github.com>
2024-01-22 12:01:15 +0530

    Update db.py

--- db.py
+++ db.py
@@ -5,7 +5,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a:5432/guviproject" # manual
 # DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a/guviproject" #internal
-DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a.oregon-postgres.render.com/guviproject?sslmode=require" #external live
+# DATABASE_URI = "postgresql://radhakrishnan:1D1sWtcoTVBB7U26tX2EOJ9a1Q4zu82x@dpg-ckp2oi8ujous738qgcdg-a.oregon-postgres.render.com/guviproject?sslmode=require" #external live
+DATABASE_URI =  "postgresql://guvi_yby8_user:MFyUGk2fbpvmiRZ8FaXIBt56uXD9eMWc@dpg-cmmvudocmk4c73e4qfh0-a.oregon-postgres.render.com:5432/guvi_yby8"
+
 # DATABASE_URI = 'postgresql://postgres:ags009@localhost:5432/curd' #local
 
 
